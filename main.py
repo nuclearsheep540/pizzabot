83f051217c952a5aea3dda7e09414ba21b988db7
TE-Good <tom.good1990@gmail.com>
2020-10-18 15:25:45 +0100

    Logged in and at menu

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
+import time
+import getpass
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 
 def print_logo():
+    """Prints inital load ASCII"""
     print("#### PIZZABOT ####")
 
 def user_details():
     """Get user details required to enter the website"""
-    username = input('Enter your username: ')
-    password = input('Enter your password: ')
-    # postcode = input('Enter your postcode: ')
-    postcode = "CM12 0HD"
-    return dict(username=username, password=password, postcode=postcode)
+    email = input('Enter your email address: ')
+    password = getpass.getpass('Enter your password: ')
+    postcode = input('Enter your postcode: ')
+    return dict(email=email, password=password, postcode=postcode)
 
-def initialize_webdriver():
-    """TODO"""
+def initialize_chrome_webdriver():
+    """Initializes the chrome webdriver"""
     driver = Chrome()
     driver.get("https://www.dominos.co.uk/")
     return driver
 
-def navigate_from_postcode_to_menu(driver, user_details):
-    """TODO"""
+def wait_for_page_load(id_string, page_name):
+    """Waits for the id string to load, or raises error message"""
     try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "search-input"))
-        )
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, id_string)))
     except:
-        print("Unable to load postcode page")
+        print(f"Unable to load {page_name} page")
         driver.quit()
+
+def navigate_from_postcode_to_menu(driver, user_details):
+    """Enter postcode and navigate to menu"""
+    wait_for_page_load(id_string="search-input", page_name="postcode")
 
     postcode_search = driver.find_element_by_id("search-input")
     postcode_search.send_keys(user_details["postcode"], Keys.RETURN)
 
 def navigate_from_menu_to_login(driver):
-    """TODO"""
-    # might need to get another ID of the menu just incase the pop-up stops showing up
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "arrival-overlay-container"))
-        )
-    except:
-        print("Unable to load menu page")
-        driver.quit()
+    """Navigate from menu to login"""
+    wait_for_page_load(id_string="arrival-overlay-container", page_name="menu")
 
     # Close pop-up
     driver.find_element_by_class_name("arrival-red").click()
@@ -53,22 +51,22 @@ def navigate_from_menu_to_login(driver):
     # Click login
     driver.find_element_by_class_name("account-link").click()
 
-def login_and_navigate_to_menu(driver):
-    """TODO"""
+def login_and_navigate_to_menu(driver, user_details):
+    """Log in with email and password, then navigate to menu"""
+    wait_for_page_load(id_string="loginPanel", page_name="login")
     
+    # Input email
+    email_input = driver.find_element_by_xpath("//input[@name='email']")
+    email_input.send_keys(user_details["email"])
 
-    
+    # Input password
+    email_input = driver.find_element_by_xpath("//input[@name='password']")
+    email_input.send_keys(user_details["password"], Keys.RETURN)
 
-# pizza_menu = #selenium get class menu-products-list
-# for "pizza" in pizza_menu, do:
-    # pizza_id = article id
+    wait_for_page_load(id_string="welcome-page", page_name="welcome")
 
-    # for (selenium get every article tag in pizza) do:
-        # return pizza_name = the innerHTML where p class = "product title"
-    
-    # return pizza(pizza_id, pizza_name)
-
-
+    # Navigate from welcome to menu
+    driver.find_element_by_id("menu-selector").click()
 
 def stop_webdriver():
     """Close webdriver"""
@@ -79,7 +77,7 @@ def stop_webdriver():
 if __name__ == "__main__":
     print_logo()
     user_details = user_details()
-    driver = initialize_webdriver()
+    driver = initialize_chrome_webdriver()
     navigate_from_postcode_to_menu(driver, user_details)
     navigate_from_menu_to_login(driver)
-    login_and_navigate_to_menu(driver)
+    login_and_navigate_to_menu(driver, user_details)
