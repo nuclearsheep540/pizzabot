96e52eddc32512321c4fd922c8bb84e1f7582d5a
TE-Good <tom.good1990@gmail.com>
2020-11-13 14:56:27 +0000

    Selected pizza, navigated to basket, and checkout

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pizzabot
 Pizza Bot
+
+
+* Takes default address from your account.
+* Must have delivery selected at `www.dominos.co.uk/mydominos` on your account for this to work.
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python3 # NOTE: DO WE NEED THIS?
 import time
 import getpass
 
-from selenium.webdriver import Chrome
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 # NOTE: To run headless
 # TODO: Add arg parser to manage this
-# from selenium import webdriver from selenium.webdriver.chrome.options import Options
-# chrome_options = Options()
 # #chrome_options.add_argument("--disable-extensions")
 # #chrome_options.add_argument("--disable-gpu")
 # #chrome_options.add_argument("--no-sandbox") # linux only
@@ -28,12 +28,15 @@ def user_details():
     password = getpass.getpass('Enter your password: ')
     postcode = input('Enter your postcode: ')
     print("\nFetching your pizza menu...\n")
+
     return dict(email=email, password=password, postcode=postcode)
 
 def initialize_chrome_webdriver():
     """Initializes the chrome webdriver"""
-    driver = Chrome()
-    driver.get("https://www.dominos.co.uk/")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.dominos.co.uk/user/login")
     return driver
 
 def wait_for_page_load(id_string, page_name):
@@ -43,20 +46,6 @@ def wait_for_page_load(id_string, page_name):
     except:
         print(f"Unable to load {page_name} page")
         driver.quit()
-
-def navigate_from_postcode_to_menu(driver, user_details):
-    """Enter postcode and navigate to menu"""
-    wait_for_page_load(id_string="search-input", page_name="postcode")
-
-    postcode_search = driver.find_element_by_id("search-input")
-    postcode_search.send_keys(user_details["postcode"], Keys.RETURN)
-
-def navigate_from_menu_to_login(driver):
-    """Closes pop-up and clicks on Login"""
-    wait_for_page_load(id_string="arrival-overlay-container", page_name="menu")
-
-    driver.find_element_by_class_name("arrival-red").click()
-    driver.find_element_by_class_name("account-link").click()
 
 def login_and_navigate_to_menu(driver, user_details):
     """Log in with email and password, then navigate to menu"""
@@ -70,13 +59,16 @@ def login_and_navigate_to_menu(driver, user_details):
 
     # NOTE: Might need an IF condition to see if are navigated to welcome or not.
     #       If we are navigated to the welcome pacge, then run this code
-    # wait_for_page_load(id_string="welcome-page", page_name="welcome")
-    
-    # driver.find_element_by_id("menu-selector").click()
+    wait_for_page_load(id_string="welcome-page", page_name="welcome")
+    driver.find_element_by_id("menu-selector").click()
 
 def get_pizza_menu(driver):
     """Constructs a list containing dicts of the pizza information"""
-    wait_for_page_load(id_string="Speciality Pizzas", page_name="menu")
+
+    ## CURRENT BUG: sometimes page loads with buy buttons for cookies, 
+    ## and 'featured article', and offsets our buy button array
+    wait_for_page_load(id_string="Pizza", page_name="menu")
+    time.sleep(1)
 
     normal_menu = driver.find_element(By.ID, 'Speciality Pizzas')
     vegan_menu = driver.find_element(By.ID, 'Vegan Friendly Pizzas')
@@ -88,42 +80,90 @@ def get_pizza_menu(driver):
 
     pizza_menu = []
     for index, pizza in enumerate(all_pizzas):
-
-        item = pizza.find_element(By.CLASS_NAME, 'h6')
-        item_name = item.get_attribute('innerHTML').replace('&amp;','&')
-        item_to_basket = item.find_element_by_xpath("//button[text()='Add To Basket']")
-
-        pizza_menu.append({"index": index, "item": item, "name": item_name, "buy": item_to_basket})
+        item = pizza.find_element(By.CLASS_NAME, "h6")
+        item_name = item.get_attribute("innerHTML").replace("&amp;","&")
+        pizza_menu.append({"index": index, "item": item, "name": item_name})
 
     return pizza_menu
 
-def order_pizza(driver):
+def select_pizza(driver):
     """Handles user choosing a pizza, and adding it to basket"""
+    time.sleep(1)
     pizza_menu = get_pizza_menu(driver)
 
     print("Choose a pizza number")
     for pizza in pizza_menu:
-        index = pizza["index"]
-        item_name = pizza["name"]
-        print(f"Pizza number {index}: {item_name}")
+        print(f"Pizza number {pizza['index']}: {pizza['name']}")
 
-    order_no = input('Enter your order number: ')
+    order_number = input("Enter your order number: ")
 
-    # Click on add to basket for the chosen pizza
-    pizza_menu[int(order_no)]["buy"].click()
+    # Click on add to basket for the chosen pizza BUG: Can only 
+    print(f"pizza {order_number} is, {pizza_menu[int(order_number)]['name']}")
 
-def stop_webdriver():
-    """Close webdriver"""
-    time.sleep(5)
-    driver.quit()
+    # get all buy buttons
+    all_buy_buttons = driver.find_element(By.ID, 'Pizza').find_elements_by_xpath("//button[text()='Add To Basket']")
+
+    # click the one relative to order number
+    all_buy_buttons[int(order_number)].click()
+
+    time.sleep(1)
+
+def navigate_to_basket(driver):
+    """Handles navigating to the basket page"""
+    driver.get("https://www.dominos.co.uk/basketdetails/show")
+
+def navigate_to_checkout(driver):
+    """Handles navigating to the checkout page"""
+    wait_for_page_load(id_string="checkoutButtonBottom", page_name="basket")
+    time.sleep(1)
+
+    basket_summary = driver.find_elements_by_class_name("basket-product-summary")
+    ordered_pizza_info = []
+    for index, item in enumerate(basket_summary):
+        spans = item.find_elements_by_tag_name("span")
+        pizza_info = [span.get_attribute("innerHTML") for span in spans]
+        pizza_info_dict = {
+            "item_index": index,
+            "name": pizza_info[0],
+            "size": pizza_info[1],
+            "crust": pizza_info[2]
+        }
+        ordered_pizza_info.append(pizza_info_dict)
+        
+    driver.find_element(By.ID, "checkoutButtonBottom").click()
+
+    return ordered_pizza_info
+    
+
+def checkout_place_order(driver, ordered_pizza_info):
+    """Confirms order"""
+    wait_for_page_load(id_string="page-content", page_name="fulfilment")
+    time.sleep(1)
+    # print the price
+    sub_total = driver.find_element_by_class_name("checkout-total-price").get_attribute("innerHTML")
+    address = driver.find_element_by_class_name("nav-store-name").get_attribute("innerHTML")
+
+    print(f"\nYour order is:")
+    for item in ordered_pizza_info:
+        print(f"{item['size']} {item['name']} with {item['crust']}")
+
+    print(f"\nYour order comes to a total of: {sub_total}\nDelivery to your {address} address")
+    confirm = input("Confirm order [y/n]? ")
+    
+    # ask for input to confirm, if yes:
+    if confirm is not "y":
+        print("Order declined. Exiting...")
+        driver.quit()
+    else:
+        driver.find_element_by_xpath("//button[@type='submit']").click()
 
 
 if __name__ == "__main__":
     print_logo()
     user_details = user_details()
     driver = initialize_chrome_webdriver()
-    navigate_from_postcode_to_menu(driver, user_details)
-    navigate_from_menu_to_login(driver)
     login_and_navigate_to_menu(driver, user_details)
-    order_pizza(driver)
+    select_pizza(driver)
+    navigate_to_basket(driver)
+    navigate_to_checkout(driver)
     # Checkout & Confirm
